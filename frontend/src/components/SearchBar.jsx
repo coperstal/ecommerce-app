cbd70e963d9769ac981c26e95e5845515bd9398e
Coperstal <papamavroudis@yahoo.gr>
2025-03-11 23:52:44 +0200

    v.1.4

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -11,7 +11,6 @@ const SearchBar = () => {
   const  location=useLocation();
   
   useEffect(()=>{
-    console.log(location.pathname)
 
     if(location.pathname.includes("collection")){
       setVisible(true)
