4758ee4fc2b71c7380c8370735094e1ec513a539
Coperstal <papamavroudis@yahoo.gr>
2025-03-12 17:48:23 +0200

    v1.6

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,13 +2,15 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import ReleatedProducts from "../components/ReleatedProducts";
+
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
-  const [size,setSize]=useState("")
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -24,7 +26,7 @@ const Product = () => {
     fetchProductData();
   }, [productId]);
 
-  console.log(productId);
+  
 
   return productData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100 ">
@@ -64,17 +66,64 @@ const Product = () => {
             {productData.price}
             {currency}
           </p>
-          <p className="mt-5 text-gray-500 md:w-4/5"> {productData.description}</p>
-            <div className="flex flex-col gap-4 my-8">
-              <p>Select Size</p>
-                <div className="flex gap-2">
-                  {productData.sizes.map((item,index)=>(
-                    <button onClick={()=>setSize(item)}  className={`border py-4 px-4 bg-gray-100 cursor-pointer ${item===size?"border-orange-500":""}`} key={index}>
-                        {item}
-                  </button>))}
-                </div>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {" "}
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-4 px-4 bg-gray-100 cursor-pointer ${
+                    item === size ? "border-orange-500" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
             </div>
+          </div>
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black px-8 py-3 text-white text-sm active:bg-gray-700 cursor-pointer">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original Product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days. </p>
+          </div>
         </div>
+      </div>
+      {/* Description and Review Section */}
+      <div className="mt-20">
+        <div className="flex ">
+          <p className="border px-5 py-3 text-sm">Description</p>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            {" "}
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem
+            enim libero fugit minima! Dolor natus, quibusdam eius quam, ut
+            assumenda eaque vel obcaecati sunt harum quos dolores velit autem
+            dolorum!
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+            harum inventore, veritatis quidem, suscipit ullam mollitia provident
+            totam quae quibusdam vitae odio vero saepe magni, fugit cumque iure
+            earum voluptas!
+          </p>
+        </div>
+        {/*Display related products */}
+
+        <ReleatedProducts
+          category={productData.category}
+          subCategory={productData.subCategory}
+        />
       </div>
     </div>
   ) : (
