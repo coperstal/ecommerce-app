092a97c9af4b111b779252da086e6526a3d70aff
Coperstal <papamavroudis@yahoo.gr>
2025-03-13 16:58:25 +0200

    v.2.0

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom"
 
 export const ShopContext = createContext();
 
@@ -10,6 +11,8 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const navigate=useNavigate()
+
 
   const addToCart = async (itemId, size) => {
     if (!size) {
@@ -55,7 +58,7 @@ const ShopContextProvider = (props) => {
     
   };
 
-  const getCartAmount=async()=>{
+  const getCartAmount=()=>{
     let totalAmount=0;
     for(const items in cartItems){
       let itemInfo=products.find((product)=>product._id===items)
@@ -69,6 +72,7 @@ const ShopContextProvider = (props) => {
         }
       }
     }
+    
       return totalAmount;
   }
 
@@ -84,7 +88,8 @@ const ShopContextProvider = (props) => {
     addToCart,
     getCartCount,
     updateQuantity,
-    getCartAmount
+    getCartAmount,
+    navigate
   };
 
   return (
