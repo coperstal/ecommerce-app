8e8b8308561d7b50dfd9ce44479271471d5bd27c
MILTIADIS PAPAMAVROUDIS <papamavroudis@yahoo.gr>
2025-03-14 17:44:30 +0200

    v2.3

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,20 @@ import cors from "cors"
 import "dotenv/config" 
 
 //App Config
+const app= express()
+const port=process.env.PORT || 4000
 
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+
+app.get('/',(req,res)=>{
+    res.send("API is working")
+})
+
+
+app.listen(port, () => {
+    console.log(`Server started on Port: ${port}`)
+  })
