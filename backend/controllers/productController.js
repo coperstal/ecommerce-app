df7a0609df6fb555a6bcf4534486d12670c29a67
MILTIADIS PAPAMAVROUDIS <papamavroudis@yahoo.gr>
2025-03-18 13:15:55 +0200

    v2.5

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+// function for add product
+const addProduct=async(req,res)=>{
+}
+
+
+// function for list products
+const listProducts=async(req,res)=>{
+    
+}
+
+//function for removing product
+const removeProduct=async(req,res)=>{
+
+}
+//function for single prduct
+const singleProduct=async(req,res)=>{
+
+}
+
+export {listProducts,addProduct,removeProduct,singleProduct}
